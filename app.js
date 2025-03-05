a688bab46b4960bbc0f58f4ad09792954365f6e2
Janina Niemelä <janina7590@hotmail.com>
2025-03-05 16:14:45 +0200

    Update app.js
    second attempt

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const html = `
           origin: { y: 0.6 },
           disableForReducedMotion: true
         });
-      }, 500);
+      }, 1000);
     </script>
     <style>
       @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
