4c804288d00713a0954b93e60245a46d168015b5
Janina Niemelä <janina7590@hotmail.com>
2025-03-05 16:11:28 +0200

    Update app.js
    testing doubling confetti

--- app.js
+++ app.js
@@ -25,6 +25,17 @@ const html = `
         });
       }, 500);
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+    <script>
+      setTimeout(() => {
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+          disableForReducedMotion: true
+        });
+      }, 500);
+    </script>
     <style>
       @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
       @font-face {
